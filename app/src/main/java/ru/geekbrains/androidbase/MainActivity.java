ef01ab1b113098eed6008e9240a994a7b139400d
kortezka <73052908+kortezka@users.noreply.github.com>
2021-07-11 23:49:23 +0300

    Homework for lesson 2

--- app/src/main/java/ru/geekbrains/androidbase/MainActivity.java
+++ app/src/main/java/ru/geekbrains/androidbase/MainActivity.java
@@ -10,46 +10,14 @@ import android.widget.Switch;
 import android.widget.Toast;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final CalendarView calendarView = findViewById(R.id.calendarView);
-
-        Button button = findViewById(R.id.button1);
-        Switch switch1 = findViewById(R.id.switch1);
-
-        if (switch1 != null) {
-            switch1.setOnCheckedChangeListener(this);
-        }
-
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                setContentView(R.layout.second_activity);
-                Button buttonBack = findViewById(R.id.button2);
-                // buttonBack
-            }
-        });
-
-
-
-        //buttonBack.setOnClickListener(v -> setContentView(R.layout.activity_main));
-    }
-
-
-    @Override
-    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-        Toast.makeText(this, "Отслеживание переключения: " + (isChecked ? "on" : "off"),
-                Toast.LENGTH_SHORT).show() ;
 
 
     }
-
 
 }
-
-
